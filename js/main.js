246bc8715ee60e6b9006abdc122a96b6188aec95
Chaos Predictor <kuzi81@gmail.com>
2017-01-17 16:16:13 +0200

    select working for a list

--- js/main.js
+++ js/main.js
@@ -2,7 +2,36 @@ var listOfChannels = ['storbeck', 'freecodecamp', 'twistgaming'];
 
 $(document).ready(function(){
 	checkAllList(listOfChannels);
+	
+	$("#o2").hide();
+
+	//checkInput();
+	checkInput2();
 });
+
+function checkInput() {
+	var timeout;
+	$("#SearchBox").on("keyup", function () {
+		var userInput = $("#SearchBox").val();
+		window.clearTimeout(timeout);
+		timeout = window.setTimeout(function() {
+			showOnlyOptionsSimilarToText($("#SelectBox"), userInput, true);
+		}, 500);
+
+	});
+}
+
+function checkInput2() {
+	var timeout;
+	$("#SearchBox").on("keyup", function () {
+		var userInput = $("#SearchBox").val();
+		window.clearTimeout(timeout);
+		timeout = window.setTimeout(function() {
+			showOnlyOptionsSimilarToText2($("#SelectList"), userInput, true);
+		}, 500);
+
+	});
+}
 
 function requestToTwitch(channel) {
 	$.getJSON('https://api.twitch.tv/kraken/streams/' + channel + '?client_id=3ayqtffruo2goxf0cvyp75wjm28g4pq&callback=?', function(data) {
@@ -17,3 +46,60 @@ function checkAllList(list){
 }
 
 
+var showOnlyOptionsSimilarToText = function (selectionEl, str, isCaseSensitive) {
+    if (typeof isCaseSensitive == 'undefined')
+        isCaseSensitive = true;
+    if (isCaseSensitive)
+        str = str.toLowerCase();
+
+    var $el = $(selectionEl);
+
+    $el.children("option:selected").removeAttr('selected');
+    $el.val('');
+    $el.children("option").hide();
+
+
+	$el.children("option").filter(function () {
+		var text = $(this).text();
+		var id= $(this).attr('id');
+		if (isCaseSensitive)
+			text = text.toLowerCase();
+		if (text.indexOf(str) > -1){
+			if (id != "o2") {
+				return true;
+			}
+		}
+			//return true;
+		return false;
+	}).show();
+
+};
+
+var showOnlyOptionsSimilarToText2 = function (selectionEl, str, isCaseSensitive) {
+    if (typeof isCaseSensitive == 'undefined')
+        isCaseSensitive = true;
+    if (isCaseSensitive)
+        str = str.toLowerCase();
+
+    var $el = $(selectionEl);
+
+    $el.children("li:selected").removeAttr('selected');
+    $el.val('');
+    $el.children("li").hide();
+
+
+	$el.children("li").filter(function () {
+		var text = $(this).text();
+		var id= $(this).attr('id');
+		if (isCaseSensitive)
+			text = text.toLowerCase();
+		if (text.indexOf(str) > -1){
+			if (id != "o2") {
+				return true;
+			}
+		}
+			//return true;
+		return false;
+	}).show();
+
+};
